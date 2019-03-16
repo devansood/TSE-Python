71f44386dc793952c44c119a27c5c9e69c1a88cd
Devan Sood <dsood1@babson.edu>
2019-03-16 11:58:29 -0400

    Day00.2
    changed calc.py one last time, changed verbage and rounded the end numbers.

--- Calc.py
+++ Calc.py
@@ -1,6 +1,6 @@
 #2.1
-minutes = input("type minutes: ")
-seconds = input("type seconds: ")
+minutes = input("Number of minutes: ")
+seconds = input("Number of seconds: ")
 totals = (int(minutes)*60+int(seconds))
 print(totals,"total seconds")
 #2.2
@@ -10,5 +10,5 @@ totalmi = (int(KMs)*1.61)
 
 print("You ran",totalmi,"miles")
 #2.3
-print("That's a total of",totals/60 / totalmi,"minutes per mile")
-print("Your average was",totalmi / totals*3600,"miles per hour")
+print("That's a total of",round(totals/60 / totalmi,3),"minutes per mile")
+print("Your average was",round(totalmi / totals*3600,3),"miles per hour")
