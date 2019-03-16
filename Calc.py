963db1ea2e5659372395dab761347aa32349f04d
Devan Sood <dsood1@babson.edu>
2019-03-16 11:50:52 -0400

    Day00.1
    slightly changed calc.py including variation of example 2

--- Calc.py
+++ Calc.py
@@ -1,17 +1,14 @@
-# print('minutes in 42 mins and 42 seconds:')
-# print("42 * 60 + 42")
-#
-# print(40 * 60 + 42)
+#2.1
 minutes = input("type minutes: ")
 seconds = input("type seconds: ")
 totals = (int(minutes)*60+int(seconds))
 print(totals,"total seconds")
-
+#2.2
 print("how many KMs")
 KMs = input()
 totalmi = (int(KMs)*1.61)
 
 print("You ran",totalmi,"miles")
-
+#2.3
 print("That's a total of",totals/60 / totalmi,"minutes per mile")
 print("Your average was",totalmi / totals*3600,"miles per hour")
