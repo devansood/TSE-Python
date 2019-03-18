6a6e0d74abb3ae38d9b430b0c2ff68d736d67863
Devan Sood <dsood1@babson.edu>
2019-03-18 14:56:17 -0400

    Day03.1
    second break including all tests so far - about 3pm

--- python tests.py
+++ python tests.py
@@ -1,51 +1,51 @@
-import turtle
-leo = turtle.Turtle()
-# def square(t, length):
-#     for i in range(4):
-#         t.fd(length)
-#         t.lt(90)
+# import turtle
+# leo = turtle.Turtle()
+# # def square(t, length):
+# #     for i in range(4):
+# #         t.fd(length)
+# #         t.lt(90)
+# #
+# # square(leo, 300)
+# # print(square)
+# # turtle.mainloop()
 #
-# square(leo, 300)
-# print(square)
-# turtle.mainloop()
-
-
-import math
+#
+# import math
+# #
+# # def circle(t, r):
+# #     circumference = 2 * math.pi * r
+# #     n = 50
+# #     length = circumference / n
+# #     polygon(t, n, length)
+# def polygon(t, n, length):
+#     angle = 360.0 / n
+#     polyline(t, n, length, angle)
+#
+# def arc(t, r, angle):
+#     arc_length = 2 * math.pi * r * angle / 360
+#     n = int(arc_length / 3) + 1
+#     step_length = arc_length / n
+#     step_angle = float(angle) / n
+#     polyline(t, n, step_length, step_angle)
+#
+# def polyline(t, n, length, angle):
+#     for i in range(n):
+#         t.fd(length)
+#         t.lt(angle)
 #
 # def circle(t, r):
-#     circumference = 2 * math.pi * r
-#     n = 50
-#     length = circumference / n
-#     polygon(t, n, length)
-def polygon(t, n, length):
-    angle = 360.0 / n
-    polyline(t, n, length, angle)
-
-def arc(t, r, angle):
-    arc_length = 2 * math.pi * r * angle / 360
-    n = int(arc_length / 3) + 1
-    step_length = arc_length / n
-    step_angle = float(angle) / n
-    polyline(t, n, step_length, step_angle)
-
-def polyline(t, n, length, angle):
-    for i in range(n):
-        t.fd(length)
-        t.lt(angle)
-
-def circle(t, r):
-    arc(t, r, 360)
-
-
-
-print(circle)
-turtle.mainloop()
-
-
-
-def square_plus_one(a):
-    return a * a + 1
-square_plus_one(2)
+#     arc(t, r, 360)
+#
+#
+#
+# print(circle)
+# turtle.mainloop()
+#
+#
+#
+# def square_plus_one(a):
+#     return a * a + 1
+# square_plus_one(2)
 
 
 # import cmath
@@ -67,21 +67,34 @@ square_plus_one(2)
 # print(quadratic1(1, 2,-3))
 
 
+#
+# def calcbmi():
+#     ht = float(input("Enter Your Height"))
+#     wt = float(input("Enter Your Weight"))
+#     calcbmi = 703*(wt / ht**2)
+#     if calcbmi <= 18.5:
+#         print("Underweight")
+#     elif calcbmi < 24.9:
+#         print("normal weight")
+#     elif calcbmi < 29.9:
+#         print("overweight")
+#     else:
+#         print("very overweight")
+#
+# calcbmi()
 
-def calcbmi():
-    ht = float(input("Enter Your Height"))
-    wt = float(input("Enter Your Weight"))
-    calcbmi = 703*(wt / ht**2)
-    if calcbmi <= 18.5:
-        print("Underweight")
-    elif calcbmi < 24.9:
-        print("normal weight")
-    elif calcbmi < 29.9:
-        print("overweight")
-    else:
-        print("very overweight")
+# totalvalue = 0
+# name = 'devan'
+# for letter in name:
+#     totalvalue = totalvalue + (ord(letter)-96)
+# print(totalvalue)
+#
+def countdown(n):
+    while n > 0:
+        print(n)
+        n = n - 1
+        import time
+        time.sleep(1)
+    print('blastoff!')
 
-calcbmi()
-
-
-
+countdown(5)
